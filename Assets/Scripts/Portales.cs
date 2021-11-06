8ff36ed9518acaf64d8dfbb274e86b544255556c
Fabian Lopez <fabianlopez928@gmail.com>
2021-11-05 19:30:02 -0500

    Se agrego codigo Portales

---
+++ Assets/Scripts/Portales.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Portales : MonoBehaviour
+{
+    public GameObject player;
+    public GameObject spwan;
+
+    void OnTriggerEnter2D (Collider2D other)
+    {
+        if (other.CompareTag("Pared"))
+        {
+
+            player.transform.position = new Vector2 (spwan.transform.position.x, player.transform.position.y);
+        }
+    }
+}
