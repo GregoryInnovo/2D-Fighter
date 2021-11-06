d7bacda797bc52b17f9b77ef4f3bf45304abe266
Fabian Lopez <fabianlopez928@gmail.com>
2021-11-05 20:58:29 -0500

    portales

--- Assets/Scripts/Portales.cs
+++ Assets/Scripts/Portales.cs
@@ -5,19 +5,29 @@ using UnityEngine;
 public class Portales : MonoBehaviour
 {
     public GameObject player;
-    public GameObject spawnX;
+    public GameObject spawnXP;
+    public GameObject spawnXM;
 
-    public GameObject spawnY;
+    public GameObject spawnYP;
+    public GameObject spawnYM;
 
     void OnTriggerEnter2D (Collider2D other)
     {
-        if (other.CompareTag("PortalX"))
+        if (other.CompareTag("PortalXP"))
         {
-            player.transform.position = new Vector2 (spawnX.transform.position.x, player.transform.position.y);
+            player.transform.position = new Vector2 (spawnXP.transform.position.x, player.transform.position.y);
         }
-        if (other.CompareTag("PortalY"))
+        if (other.CompareTag("PortalXM"))
         {
-            player.transform.position = new Vector2(player.transform.position.x, spawnY.transform.position.y);
+            player.transform.position = new Vector2(spawnXM.transform.position.x, player.transform.position.y);
         }
-    }
+            if (other.CompareTag("PortalYP"))
+        {
+            player.transform.position = new Vector2(player.transform.position.x, spawnYP.transform.position.y);
+        }
+        if (other.CompareTag("PortalYM"))
+        {
+            player.transform.position = new Vector2(player.transform.position.x, spawnYM.transform.position.y);
+        }
+    }// como funciona los teleporst
 }
