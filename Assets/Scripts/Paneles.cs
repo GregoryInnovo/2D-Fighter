181737231c9bd9ec4f8372957a31058c7cfd5f7b
Gregson Murcia Castro <gregoryinnovo@gmail.com>
2021-12-02 13:01:46 -0500

    toggleActive players implementation

--- Assets/Scripts/Paneles.cs
+++ Assets/Scripts/Paneles.cs
@@ -12,29 +12,41 @@ public class Paneles : MonoBehaviour
     [SerializeField] Text text_Timer;
     [SerializeField] float timer;
 
+    private GameObject player1, player2;
+    private bool auxActivePlayers;
+
     private void Start()
     {
         panelGameOver.SetActive(false);
 
+        // Encuentra el gameObject
+        player1 = GameObject.Find("Player_1");
+        player2 = GameObject.Find("Player_2");   
+        auxActivePlayers = true;    
     }
+    
     private void Update()
-    { // timer inicio
-        
+    { 
+        // timer inicio
         if (vidaplayer1.health <= 0)
         {
             panelGameOver.SetActive(true);
             textGameOver.text = "Player 1 ha perdido";
+            // Desactivar players
+            ToggleDisablePlayer();
         }
         if(vidaplayer2.health <= 0)
         {
             panelGameOver.SetActive(true);
             textGameOver.text = "Player 2 ha perdido";
+            // Desactivar players
+            ToggleDisablePlayer();
         }
+
         Timer();
     }
     public void Timer()
     {
-
         if (timer >= 0)
         {
             text_Timer.text = timer.ToString("0");
@@ -42,7 +54,6 @@ public class Paneles : MonoBehaviour
         }
         else
         {
-
             text_Timer.text = ("¡A Jugar!");
             StartCoroutine(espera());
         }
@@ -52,6 +63,16 @@ public class Paneles : MonoBehaviour
     {
         yield return new WaitForSeconds(1f);
         Destroy(text_Timer);
+        if(auxActivePlayers) {
+            // Activar players
+            ToggleDisablePlayer();
+            auxActivePlayers= false;
+        }
+    }
+
+    void ToggleDisablePlayer() {
+        player1.GetComponent<Salto>().enabled = !player1.GetComponent<Salto>().enabled;
+        player2.GetComponent<Salto>().enabled = !player2.GetComponent<Salto>().enabled;
     }
 
 }
